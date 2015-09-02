e4dcc3417c20eddcf86e0214b95ec76510c04e47
asimha <asimha@qwinixtech.com>
2015-09-02 17:43:10 +0530

    Git hub open PR gem

---
+++ git_pr.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'git_open_pr'
+  s.version     = '0.0.0'
+  s.date        = '2015-09-02'
+  s.summary     = "Fetches the git hub open PR"
+  s.description = "recent first"
+  s.authors     = ["Akshaysimha"]
+  s.email       = 'akshay.simha91@gmail.com'
+  s.files       = ["lib/git_pr.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/git_pr'
+  s.license       = 'MIT'
+end
---
+++ lib/git_pr.rb
@@ -0,0 +1,24 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+class GitPr
+  def self.github_pr(organisation_name, access_token, repo_name)
+    uri = URI.parse("https://api.github.com/repos/#{organisation_name}/#{repo_name}/pulls?state=open")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "token #{access_token}"
+    @response = http.request(request)
+    pull = JSON.parse(@response.body)
+    pulls = []
+    pull.each do |p|
+      @title = p["title"]
+      @creator = p["user"]["login"]
+      @updated = p["updated_at"]
+      
+      pulls << {"title" => @title, "creator" => @creator, "updated at" => @updated}
+    end
+     pulls
+  end
+end
